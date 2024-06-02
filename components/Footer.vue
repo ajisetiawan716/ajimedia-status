0a7d9ef261d5c0cc4a7002237ab51abdc40add79
Aji Setiawan <106381048+ajisetiawan716@users.noreply.github.com>
2024-06-02 19:34:30 +0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="flex px-10 mt-16 mx-auto max-w-4xl">
       <div class="flex w-full pt-4 border-t border-gray-light">
         <span class="text-dark">Crafted by <NuxtLink class="text-link" to="https://github.com/skaiwlkr">Sebastian Kaier</NuxtLink></span>
-        <NuxtLink class="ml-auto text-link" to="https://github.com/skaiwlkr/better-uptime-status-page" target="_blank" title="See on GitHub">GitHub</NuxtLink>
+        <span class="text-dark">Powered by <NuxtLink class="ml-auto text-link" to="https://www.ajisetiawan.my.id" target="_blank" title="See on GitHub">Aji Setiawan</NuxtLink></span>
       </div>
     </div>
   </div>
