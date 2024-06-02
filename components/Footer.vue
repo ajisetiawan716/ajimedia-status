742a57cc8050d23b152d226aa6ae04c07ac5ed19
Aji Setiawan <106381048+ajisetiawan716@users.noreply.github.com>
2024-06-02 19:41:54 +0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,8 +2,7 @@
   <div>
     <div class="flex px-10 mt-16 mx-auto max-w-4xl">
       <div class="flex w-full pt-4 border-t border-gray-light">
-        <span class="text-dark">Crafted by <NuxtLink class="text-link" to="https://github.com/skaiwlkr">Sebastian Kaier</NuxtLink></span>
-        <span class="text-dark">Powered by <NuxtLink class="ml-auto text-link" to="https://www.ajisetiawan.my.id" target="_blank" title="See on GitHub">Aji Setiawan</NuxtLink></span>
+        <span class="text-dark">Poeered by <NuxtLink class="text-link" to="https://www.ajisetiawan.my.id">Aji Setiawan</NuxtLink></span>
       </div>
     </div>
   </div>
