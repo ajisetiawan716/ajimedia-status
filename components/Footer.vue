e9bfdbb87efc84753cbdf49392548370c02520e4
Aji Setiawan <106381048+ajisetiawan716@users.noreply.github.com>
2024-06-03 08:10:41 +0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="flex px-10 mt-16 mx-auto max-w-4xl">
       <div class="flex w-full pt-4 border-t border-gray-light">
         <span class="text-dark">&copy; 2024 <b>Aji Media.</b></span>
-        <span class="text-dark">Powered by <NuxtLink class="text-link" to="https://www.ajisetiawan.my.id">Aji Setiawan</NuxtLink></span>
+        <span class="ml-auto text-dark">Powered by <NuxtLink class="text-link" to="https://www.ajisetiawan.my.id">Aji Setiawan</NuxtLink></span>
       </div>
     </div>
   </div>
